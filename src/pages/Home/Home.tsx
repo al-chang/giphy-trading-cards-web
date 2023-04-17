00c8ce6cffd3f1b5a201a785071910f2712f6c1f
Alexander Chang <56360563+al-chang@users.noreply.github.com>
2023-04-16 23:35:26 -0400

    Followers (#4)
    * add way to follow other users
    
    * Basic way to view followers cards

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import { useUserContext } from "../../hooks/useUser";
+import { getCards } from "../../services/cardService";
+import { TCard } from "../BrowseCards/BrowseCards";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
   const { user } = useUserContext();
+  const [cards, setCards] = useState<TCard[]>([]);
 
-  return <div>You are on the home page</div>;
+  const loadData = async () => {
+    const data = await getCards(user ? { following: true } : {});
+    setCards(data.data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      {cards?.map((card) => (
+        <Card key={card.id} {...card} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
